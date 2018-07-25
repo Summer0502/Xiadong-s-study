53cea63c862f8b57944ad469c0bf0877ba1dc539
Summer0502 <38901221+Summer0502@users.noreply.github.com>
2018-07-25 16:42:37 +0800

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -27,5 +27,3 @@ print(prediction)
 
 #这里，  0 对应 Versicolor（杂色鸢尾） 1 对应 Virginica（维吉尼亚鸢尾） 2 对应 Setosa（山鸢尾）
 # 基于给定输入，使用 KNN 分类器，两张图中的花都被预测为 Virginica。
-
-#原文链接https://mp.weixin.qq.com/s?__biz=MzA3MzI4MjgzMw==&mid=2650737297&idx=3&sn=cc16437f343cac2d2eabdd8a9205c87a&chksm=871aceefb06d47f90233a703711385db2e1dc807876740bf10d50b785f59ec42a3199c83be86&mpshare=1&scene=23&srcid=07030hPBZntEE0DWIvfu8n9I#rd
