b8c48d7e0663a48b3bfccee6d29197e91bb99937
Summer0502 <38901221+Summer0502@users.noreply.github.com>
2018-07-25 16:38:53 +0800

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,29 @@
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+# Load iris dataset from sklearn
+iris = datasets.load_iris()
+
+# Declare an of the KNN classifier class with the value with neighbors.
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the model with training data and target values
+knn.fit(iris['data'], iris['target'])
+
+# Provide data whose class labels are to be predicted
+X = [
+    [5.9, 1.0, 5.1, 1.8],
+    [3.4, 2.0, 1.1, 4.8],
+]
+
+# Prints the data provided
+print(X)
+
+# Store predicted class labels of X
+prediction = knn.predict(X)
+
+# Prints the predicted class labels of X
+print(prediction)
+
+#这里，  0 对应 Versicolor（杂色鸢尾） 1 对应 Virginica（维吉尼亚鸢尾） 2 对应 Setosa（山鸢尾）
+# 基于给定输入，使用 KNN 分类器，两张图中的花都被预测为 Virginica。
